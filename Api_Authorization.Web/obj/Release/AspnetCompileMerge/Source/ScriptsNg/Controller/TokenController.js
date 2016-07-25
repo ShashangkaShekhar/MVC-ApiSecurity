b419f97c3f984a9e8ca29d9b4eba4cda04e883a5
Shashangka Shekhar <shashangka@gmail.com>
2016-07-25 15:49:55 +0600

    MVC Api Security

---
+++ Api_Authorization.Web/obj/Release/AspnetCompileMerge/Source/ScriptsNg/Controller/TokenController.js
@@ -0,0 +1,22 @@
+﻿
+AppSecurity.controller('tokenCtrl', ['$scope', '$http', 'crudService', '$sessionStorage',
+function ($scope, $http, crudService, $sessionStorage) {
+
+    //Token Generate
+    $scope.tokenManager = {
+        generateSecurityToken: function (methodtype) {
+            var username = $sessionStorage.loggeduser;
+            var key = methodtype;
+            var ip = $sessionStorage.loggedip;
+            var userAgent = navigator.userAgent.replace(/ \.NET.+;/, '');
+            var message = [username, ip, userAgent].join(':');
+            var hash = CryptoJS.HmacSHA256(message, key);
+            var token = CryptoJS.enc.Base64.stringify(hash);
+            var tokenId = [username, key].join(':');
+            var tokenStr = CryptoJS.enc.Utf8.parse([token, tokenId].join(':'));
+
+            //Token Return
+            return CryptoJS.enc.Base64.stringify(tokenStr);
+        },
+    };
+}]);
---
+++ Api_Authorization.Web/obj/Release/Package/PackageTmp/ScriptsNg/Controller/CustomerController.js
@@ -0,0 +1,38 @@
+﻿
+AppSecurity.controller('customerCtrl', ['$scope', '$http', 'crudService',
+function ($scope, $http, crudService) {
+
+    $scope.customerManager = {
+        pagesize: 100,
+        pagenumber: 0,
+        custurl: '/api/Customer/GetCustomers',
+        methodtype: null,
+
+        //Http Get Customer 
+        getcustomer: function () {
+            $scope.customers = [];
+            $scope.customerManager.methodtype = 'get';
+            var headerToken = { 'AuthorizedToken': $scope.tokenManager.generateSecurityToken($scope.customerManager.methodtype) };
+
+            //Service call
+            var url = $scope.customerManager.custurl + '/' + $scope.customerManager.pagenumber + '/' + $scope.customerManager.pagesize;
+            var cust = crudService.get(url, headerToken);
+            cust.then(function (response) {
+                $scope.result = 'Customer records(' + response.data.customer.length + ') : HTTP-' + response.status;
+                $scope.customers = response.data.customer;
+            },
+            function (error) {
+                $scope.result = 'Limited access(Unauthorized) : HTTP-' + error.status;
+            });
+        },
+        savecustomer: function () {
+            $scope.customerManager.methodtype = 'post';
+        },
+        updatecustomer: function () {
+            $scope.customerManager.methodtype = 'put';
+        },
+        deletecustomer: function () {
+            $scope.customerManager.methodtype = 'delete';
+        },
+    };
+}]);
